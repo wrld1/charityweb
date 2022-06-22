c92e3cf8ea61a9aa274aa5fcac300f31f2b73015
kyrylo.lolenko <kir260302@gmail.com>
2022-06-23 01:14:40 +0300

    multilang

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const select = document.querySelector("select");
+const allLang = ["ua", "en", "es"];
+
+select.addEventListener("change", changeURLLanguage);
+
+function changeURLLanguage() {
+  let lang = select.value;
+  location.href = window.location.pathname + "#" + lang;
+  location.reload();
+}
+
+function changeLanguage() {
+  console.log("hui");
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + "#ua";
+    location.reload();
+  }
+  select.value = hash;
+  document.querySelector("title").innerHTML = langArr["title"][hash];
+
+  //   document.querySelector(".li-main").innerHTML = langArr["liMain"][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector(".lng-" + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
+    }
+    // document.querySelector(".lng-" + key).innerHTML = langArr[key][hash];
+  }
+}
+changeLanguage();
---
+++ js/lang.js
@@ -0,0 +1,194 @@
+const langArr = {
+  navMain: {
+    ua: "Головна",
+    en: "Main Page",
+    es: "página de inicio",
+  },
+  navVictims: {
+    ua: "Постраждалі",
+    en: "Victims",
+    es: "Victimos",
+  },
+  navVictims1Item: {
+    ua: "1 Постраждалий",
+    en: "1 Victim ",
+    es: "1 Victimos",
+  },
+  navVictims2Item: {
+    ua: "2 Постраждалий",
+    en: "2 Victim ",
+    es: "2 Victimos",
+  },
+  navVictims3Item: {
+    ua: "3 Постраждалий",
+    en: "3 Victim ",
+    es: "3 Victimos",
+  },
+  navProjects: {
+    ua: "Проекти",
+    en: "Projects ",
+    es: "Projectos",
+  },
+  navAboutUs: {
+    ua: "Про нас",
+    en: "About us ",
+    es: "sobre nosotros",
+  },
+  navDonateBtn: {
+    ua: "Допомогти",
+    en: "Donate",
+    es: "donar",
+  },
+  mainTitle: {
+    ua: "Новітнє життя України",
+    en: "New Life Of Ukraine",
+    es: "New Life Of Ukraine",
+  },
+  buttonSupport: {
+    ua: "Підтримати",
+    en: "Donate",
+    es: "Donar",
+  },
+  mainText: {
+    ua: "Новітнє життя України - організація Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "New Life Of Ukraine - organisation Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "New Life Of Ukraine - la organizacion Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+
+  missionTitle: {
+    ua: "Наша <span>місія</span>",
+    en: "Our <span>mission</span>",
+    es: "Nuestra <span>misión</span>",
+  },
+  missionText: {
+    ua: "Наша місія - організація Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Our mission - organisation Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Nuestra misión - la organizacion Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  victimsTitle: {
+    ua: "Постраждалі",
+    en: "Victims",
+    es: "Las víctimas",
+  },
+  victimsSlideText1: {
+    ua: "Постраждалі lorem Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Victims Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Las víctimas Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  victimsSlideText2: {
+    ua: "Постраждалі lorem Fugit ipsam culpa at ducimus, eligendi nam voluptas!",
+    en: "Victims Fugit ipsam culpa at ducimus, eligendi nam voluptas!",
+    es: "Las víctimas Fugit ipsam culpa at ducimus, eligendi nam voluptas! ",
+  },
+  victimsSlideText3: {
+    ua: "Постраждалі lorem Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Victims Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Las víctimas Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  donateTitle: {
+    ua: "Як <span>допомогти</span>",
+    en: "<span>How</span> you can help",
+    es: "Pueden <span>ayudar</span>",
+  },
+  donateSubtitle: {
+    ua: "Пожертвування",
+    en: "Donate",
+    es: "Donar",
+  },
+  donateBlockText: {
+    ua: "Пожертвування Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Donate Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Donar Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  donateButtonSupport: {
+    ua: "Підтримати",
+    en: "Donate",
+    es: "Donar",
+  },
+  GITSubtitle: {
+    ua: "Гуманітарна допомога",
+    en: "Humanitarian help",
+    es: "Asistencia humanitaria",
+  },
+  GITName: {
+    ua: `Ім'я:
+    <input type="text" id="name" placeholder="Ваше ім'я" required name="Ім'я" />`,
+    en: `Your name <input type="text" id="name" placeholder="Your name" required name="Name" />`,
+    es: `Su nombre <input type="text" id="name" placeholder="Su nombre" required name="Ім'я" />`,
+  },
+  GITEmail: {
+    ua: `Ваш email:<input type="email" class="form_input" id="email" placeholder="Email" required name="email"/>`,
+    en: `Your email:<input type="email" class="form_input" id="email" placeholder="Email" required name="email"/>`,
+    es: `Tu email:<input type="email" class="form_input" id="email" placeholder="Email" required name="email"/>`,
+  },
+  GITPhone: {
+    ua: `Номер телефону:<input type="text" id="phone" placeholder="Номер телефону" required name="Номер телефону"/>`,
+    en: `Phone number:<input type="text" id="phone" placeholder="Phone number" required name="Номер телефону"/>`,
+    es: `Número de teléfono:<input type="text" id="phone" placeholder="Número de teléfono" required name="Номер телефону"/>`,
+  },
+  GITMessage: {
+    ua: `Ваше повідомлення: <textarea id="message" rows="4" placeholder="Ваше повідомлення" name="Повідомлення"></textarea>`,
+    en: `Your message: <textarea id="message" rows="4" placeholder="Your message" name="Повідомлення"></textarea>`,
+    es: `Su mensaje: <textarea id="message" rows="4" placeholder="Su mensaje" name="Повідомлення"></textarea>`,
+  },
+  GITSubmit: {
+    ua: "Надіслати",
+    en: "Submit",
+    es: "Enviar",
+  },
+  projectsTitle: {
+    ua: "Наші <span>проекти</span>",
+    en: "Our <span>projects</span>",
+    es: "Nuestros <span>proyectos</span>",
+  },
+  projectsSubTitle1: {
+    ua: "Проект 1",
+    en: "Project 1",
+    es: "Proyecto 1",
+  },
+  projectsSubTitle2: {
+    ua: "Проект 2",
+    en: "Project 2",
+    es: "Proyecto 2",
+  },
+  projectsSubTitle3: {
+    ua: "Проект 3",
+    en: "Project 3",
+    es: "Proyecto 3",
+  },
+  projectsText1: {
+    ua: "Проект 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Project 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Proyecto 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  projectsText2: {
+    ua: "Проект 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Project 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Proyecto 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  projectsText3: {
+    ua: "Проект 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Project 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Proyecto 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  aboutUsTitle: {
+    ua: "Про <span>нас</span>",
+    en: "About <span>us</span>",
+    es: "Sobre <span>nosotros</span>",
+  },
+  aboutUsMission: {
+    ua: "Наше завдання Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Our mission Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Nuestra misión Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  aboutUsTeam: {
+    ua: "Наша команда Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    en: "Our Team Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    es: "Nuestro equipo Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+  },
+  title: {
+    ua: "Новітнє життя України",
+    en: "New Life Of Ukraine",
+    es: "New Life Of Ukraine",
+  },
+};
