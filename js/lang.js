92c42f67bfb426763ec8ae16766964c5ac7aed7a
kyrylo.lolenko <kir260302@gmail.com>
2022-07-02 02:55:10 +0300

    loadspeed fix

--- js/lang.js
+++ js/lang.js
@@ -50,7 +50,7 @@ const langArr = {
     es: "Donar",
   },
   mainText: {
-    ua: "Новітнє життя України - організація Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "БФ «Новітнє життя України» – є благодійним фондом, метою якого є  надання благодійної допомоги фізичним особам, територіальним громадам, неприбутковим організаціям, а також юридичним особам приватного права в указаних сферах, а також розвиток і підтримка цих сфер у суспільних інтересах; сприяння захисту громадян в період надзвичайних ситуацій воєнного і мирного стану, у тому числі із залученням іноземних благодійників – фондів, організацій, установ, фізичних осіб, а також сприяння реалізації програм і заходів, направлених на відновлення зруйнованого житла та інфраструктури України. ",
     en: "New Life Of Ukraine - organisation Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "New Life Of Ukraine - la organizacion Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
@@ -61,7 +61,7 @@ const langArr = {
     es: "Nuestra <span>misión</span>",
   },
   missionText: {
-    ua: "Наша місія - організація Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "Наша місія -  допомагати постраждалим українцям, які внаслідок війни втратили близьких людей, залишились без житла та майна, реалізовувати благодійні ініціативи, направлені на допомогу цим людям та відбудову їх  будинків, залучати до допомоги інших людей та організації.  Україна – єдина родина, а якщо в цій родині хтось потребує допомоги, то ми не повинні залишати сам на сам людей з їх проблемами.",
     en: "Our mission - organisation Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Nuestra misión - la organizacion Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum recusandae veritatis cumque optio necessitatibus similique labore, tempora repellendus rerum iusto illo quis. Fugit ipsam culpa at ducimus, eligendi nam voluptas! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
@@ -157,17 +157,17 @@ const langArr = {
     es: "Proyecto 3",
   },
   projectsText1: {
-    ua: "Проект 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "Метою проекту є відбудова для жителів Київщини та Чернігівщини  їх  будинків, які були  повністю зруйновані в результаті влучання російських снарядів ",
     en: "Project 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Proyecto 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
   projectsText2: {
-    ua: "Проект 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "Метою проекту є організація та проведення благодійних аукціонів, на яких реалізовуватися лоти, які були передані БФ «Новітнє життя України», а гроші від їх продажу будуть йти на відбудову  житла для мешканців Київщини та Чернігівщини ",
     en: "Project 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Proyecto 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
   projectsText3: {
-    ua: "Проект 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "Надання психологічної допомоги українцям під час війни Метою проекту є надання кваліфікованими психологами з Асоціації психологів України психологічної допомоги особам, які потребують її отримання внаслідок війни  та формування у громадян навичок первинної психологічної допомоги і самодопомоги під час війни",
     en: "Project 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Proyecto 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
@@ -177,12 +177,12 @@ const langArr = {
     es: "Sobre <span>nosotros</span>",
   },
   aboutUsMission: {
-    ua: "Наше завдання Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "Сприяння здійсненню міжнародних, національних, регіональних і місцевих програм, спрямованих на розвиток освіти, охорону здоров’я, захист екології, охорону довкілля та захист тварин, запобігання природним і техногенним катастрофам та ліквідація їх наслідків, допомогу постраждалим внаслідок катастроф, збройних конфліктів і нещасних випадків, а також біженцям та особам, які перебувають у складних життєвих обставинах, у тому числі відновлення зруйнованого житла та інфраструктури України.",
     en: "Our mission Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Nuestra misión Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
   aboutUsTeam: {
-    ua: "Наша команда Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
+    ua: "БФ «Новітнє життя України» був створений у травні 2022 року небайдужими киянами.",
     en: "Our Team Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
     es: "Nuestro equipo Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates cumque eiusnam ea unde dolorum, officia tempore sed repudiandae nemo assumenda vitae nesciunt tempora dicta expedita error iusto enim alias.",
   },
@@ -195,5 +195,10 @@ const langArr = {
     ua: "Новини",
     en: "News",
     es: "Nuevos",
+  },
+  newsText: {
+    ua: "Відома українська художниця ____________________передала для продажу на благодійному аукціоні для підтримки родини Логвіненко Світлани Володимирівни картини «___________________».",
+    en: "Відома українська художниця ____________________передала для продажу на благодійному аукціоні для підтримки родини Логвіненко Світлани Володимирівни картини «___________________».",
+    es: "Відома українська художниця ____________________передала для продажу на благодійному аукціоні для підтримки родини Логвіненко Світлани Володимирівни картини «___________________».",
   },
 };
--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,6 @@
 var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
   cssMode: false,
   navigation: {
     nextEl: ".swiper-button-next",
@@ -6,6 +8,7 @@ var swiper = new Swiper(".mySwiper", {
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
   keyboard: true,
 });
